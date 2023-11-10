c4eddee5854929698c6d28cbf5880395cbd2f8cb
Roman <panov.rom@yandex.ru>
2023-11-10 19:37:43 +0300

    31.5_Smart ObjectPage.

---
+++ tests/test_auth_page.py
@@ -0,0 +1,15 @@
+import pytest
+from pages.auth_page import AuthPage
+
+
+def test_authorisation(web_browser):
+
+    page = AuthPage(web_browser)
+
+    page.email.send_keys('pihianov.rom@yandex.ru')
+
+    page.password.send_keys("12345")
+
+    page.btn.click()
+
+    assert page.get_current_url() == 'https://petfriends.skillfactory.ru/all_pets'
